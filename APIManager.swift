45d8bc2dbdc02a71a54f359e70cec8c0d54003a9
bizofitmobile <info@bizofit.com>
2020-05-25 12:18:53 +0530

    added framework

---
+++ APIManager.swift
@@ -0,0 +1,20 @@
+//
+//  APIManager.swift
+//  TestFramwork
+//
+//  Created by Arpit Lokwani on 25/05/20.
+//  Copyright © 2020 Arpit Lokwani. All rights reserved.
+//
+
+import Foundation
+
+public class APIManager{
+    
+    
+    public init() {
+        
+    }
+  public  func testFunction(){
+        print("test")
+    }
+}
---
+++ TestFramwork/TestFramwork.h
@@ -0,0 +1,19 @@
+//
+//  TestFramwork.h
+//  TestFramwork
+//
+//  Created by Arpit Lokwani on 25/05/20.
+//  Copyright © 2020 Arpit Lokwani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFramwork.
+FOUNDATION_EXPORT double TestFramworkVersionNumber;
+
+//! Project version string for TestFramwork.
+FOUNDATION_EXPORT const unsigned char TestFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramwork/PublicHeader.h>
+
+
